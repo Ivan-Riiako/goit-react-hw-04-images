720129a7fb2b2d767c56dd1b9b817779657f13c6
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2024-02-13 11:21:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import style from './App.module.css';
 
@@ -11,93 +11,119 @@ import ImageGallery from './ImageGallery';
 import Loader from 'components/Loader';
 import Button from 'components/Button';
 
-class App extends Component {
-  state = {
-    query: '',
-    arrayPictures: [],
-    error:null,
-    isLoading: false,
-    currentPage: 1,
-    totalPage: 0,
-  };
-  async componentDidUpdate(_, prevState) {
-    const { currentPage, query } = this.state;
-
-    if (prevState.query !== query || prevState.currentPage !== currentPage) {
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [arrayPictures, setArrayPictures] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    async function fethQuery() {
       try {
-        this.setState({
-          isLoading: true,
-        });
+        setIsLoading(true);
+        setError(null);
         const queryPure = queryNormalaize(query);
         const arrayImages = await API.fetchPhoto(`${queryPure}`, currentPage);
         const { arrayPictures, totalPage } = imagesArreyNormalaize(arrayImages);
 
         if (arrayPictures.length === 0) {
-
           toast('no images found, try again!', {
             style: {
               backgroundColor: 'rgba(209, 191, 53, 0.2',
             },
           });
-          this.setState({
-            error:true,
-          });
+          setError(true);
           return;
         }
 
-        this.setState(prevState => ({
-          arrayPictures: [...prevState.arrayPictures, ...arrayPictures],
-          totalPage,
-          error:null
-        }));
+        setArrayPictures(prevArray => [...prevArray, ...arrayPictures]);
+        setTotalPage(totalPage);
       } catch (error) {
-        this.setState({
-          error
-        });
         toast.error('This is an error!');
+        setError(error);
       } finally {
-        this.setState({
-          isLoading: false,
-        });
+        setIsLoading(false);
       }
     }
-  }
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    }));
+    fethQuery();
+  }, [query, currentPage]);
+
+  //  componentDidUpdate(_, prevState) {
+
+  //   const { currentPage, query } = this.state;
+
+  //   if (prevState.query !== query || prevState.currentPage !== currentPage) {
+  //     try {
+  //       this.setState({
+  //         isLoading: true,
+  //       });
+  //       const queryPure = queryNormalaize(query);
+  //       const arrayImages = await API.fetchPhoto(`${queryPure}`, currentPage);
+  //       const { arrayPictures, totalPage } = imagesArreyNormalaize(arrayImages);
+
+  //       if (arrayPictures.length === 0) {
+
+  //         toast('no images found, try again!', {
+  //           style: {
+  //             backgroundColor: 'rgba(209, 191, 53, 0.2',
+  //           },
+  //         });
+  //         this.setState({
+  //           error:true,
+  //         });
+  //         return;
+  //       }
+
+  //       this.setState(prevState => ({
+  //         arrayPictures: [...prevState.arrayPictures, ...arrayPictures],
+  //         totalPage,
+  //         error:null
+  //       }));
+  //     } catch (error) {
+  //       this.setState({
+  //         error
+  //       });
+  //       toast.error('This is an error!');
+  //     } finally {
+  //       this.setState({
+  //         isLoading: false,
+  //       });
+  //     }
+  //   }
+  // };
+
+  const handleLoadMore = () => {
+    setCurrentPage(prev => prev + 1);
   };
-  handleSubmit = data => {
+
+  const handleSubmit = data => {
     if (data === '') {
       toast('Please enter a search query');
-      this.setState({
-        arrayPictures: [],
-        totalPage: 0,
-      });
+      setArrayPictures([]);
+      setTotalPage(0);
       return;
     }
-
-    this.setState({
-      query: `${Date.now()}/${data}`,
-      arrayPictures: [],
-      currentPage: 1,
-    });
+    setCurrentPage(1);
+    setArrayPictures([]);
+    setQuery(`${Date.now()}/${data}`);
   };
 
-  render() {
-    const { arrayPictures, isLoading, currentPage, totalPage,error } = this.state;
-    const { handleSubmit, handleLoadMore } = this;
-    const isLoadMore = currentPage < totalPage && !isLoading && !error; 
-    return (
-      <div className={style.App}>
-        <Searchbar onSubmit={handleSubmit} />
-        {arrayPictures.length!==0 && <ImageGallery arrayPictures={arrayPictures} />}
-        <Toaster />
-        {isLoading && <Loader />}
-        {isLoadMore && <Button onLoadMore={handleLoadMore} />}
-      </div>
-    );
-  }
-}
+  const isLoadMore = currentPage < totalPage && !isLoading && !error;
+  return (
+    <div className={style.App}>
+      <Searchbar onSubmit={handleSubmit} />
+      {arrayPictures.length !== 0 && (
+        <ImageGallery arrayPictures={arrayPictures} />
+      )}
+      <Toaster />
+      {isLoading && <Loader />}
+      {isLoadMore && <Button onLoadMore={handleLoadMore} />}
+    </div>
+  );
+};
 
 export default App;
